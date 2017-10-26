0d41053f256c12d35b70c213df0f47dabdd45563
Deployment Bot (from Travis CI) <willhanchen@gmail.com>
2017-10-26 09:39:51 +0000

    Deploy weihanchen/angular-input-select-all to github.com/weihanchen/angular-input-select-all.git:gh-pages

---
+++ js/plugins/angular-input-select-all/angular-input-select-all.js
@@ -0,0 +1,38 @@
+/*!
+ * ngInputSelectAll v0.1.0
+ * https://weihanchen.github.io/angular-input-select-all
+ *
+ * License: MIT
+ *
+ * Generated at 2017-10-26 09:39:37 +0000
+ */
+/**
+ * @ngdoc directive
+ * @name inputSelectAll
+ * @module angular-input-select-all
+ *
+ * @description
+ * Simple input select all, prevent interrupted user's operation
+ */
+(function() {
+      angular.module('angular-input-select-all', [])
+      .directive('inputSelectAll', [inputSelectAll]);
+
+   function inputSelectAll() {
+      return {
+         restrict: 'A',
+         controller: ['$element', '$timeout', selectAllController],
+         controllerAs: 'selectAllCtrl',
+         bindToController: true
+      };
+
+      function selectAllController($element, $timeout) {
+         $element.on('click', function() {
+            var isInputSelected = $element[0].selectionEnd != $element[0].selectionStart;
+            if (!isInputSelected) {
+               $element[0].select();
+            }
+         });
+      }
+   }
+})();
